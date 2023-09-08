805a5baaef623ef70432784af55e71649f07164f
unknown <irinadaniela.geanta@gmail.com>
2023-09-08 21:33:25 +0300

    curs2 final

---
+++ src/curs2/Rectangle.java
@@ -0,0 +1,10 @@
+package curs2;
+
+public class Rectangle {
+    public static void main(String[] args) {
+
+        int length = 3;
+        int width = 6;
+        System.out.println("Aria este: " + length*width);
+    }
+}
